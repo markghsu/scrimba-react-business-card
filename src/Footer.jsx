6d979997eebe80e021cc7494f0e7f8b8967c6045
Mark Hsu <mark.g.hsu@gmail.com>
2024-01-20 12:58:59 -0800

    Feat: Add Footer and social icons

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import './Footer.css'
+import twitter from './assets/twitter.svg'
+import facebook from './assets/facebook.svg'
+import github from './assets/github.svg'
+import instagram from './assets/instagram.svg'
 
 export default function Footer() {
     return (
-        <h1>Footer goes here</h1>
+        <footer className="footer">
+            <a href="https://twitter.com/markghsu" className="footer--link">
+                <img className="footer--icon" src={twitter} alt="Twitter Logo" />
+            </a>
+            <a href="https://facebook.com" className="footer--link">
+                <img className="footer--icon" src={facebook} alt="Facebook Logo" />
+            </a>
+            <a href="https://github.com/markghsu" className="footer--link">
+                <img className="footer--icon" src={github} alt="Github Logo" />
+            </a>
+            <a href="https://instagram.com" className="footer--link">
+                <img className="footer--icon" src={instagram} alt="Instagram Logo" />
+            </a>
+        </footer>
     )
 }
