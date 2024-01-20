e7fa32735309dd9dd1db71a40830796ed57bd39b
Mark Hsu <mark.g.hsu@gmail.com>
2024-01-20 12:11:13 -0800

    Add App component and initial css

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'normalize.css';
+import './index.css'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <h1>Hello</h1>
+        <App />
     </React.StrictMode>,
 )
