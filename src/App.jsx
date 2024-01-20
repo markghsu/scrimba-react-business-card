cf91e877d0ddb708e50eb979c2647f72220ce0dd
Mark Hsu <mark.g.hsu@gmail.com>
2024-01-20 12:27:38 -0800

    Separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Info from './Info'
+import About from './About'
+import Interests from './Interests'
+import Footer from './Footer'
+
 import './App.css'
 
 export default function App() {
     return (
         <div className="app">
-            <h1>Goodbye</h1>
+            <Info />
+            <About />
+            <Interests />
+            <Footer />
         </div>
     )
 }
